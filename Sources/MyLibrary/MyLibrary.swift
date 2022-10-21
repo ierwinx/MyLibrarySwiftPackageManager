ab084f3595e578808a8a733fa1b8bd17a26caa21
Eяw!и Gontier <ierwinx@me.com>
2022-10-21 12:11:40 -0500

    se crea un framework con SPM

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,6 +1,14 @@
-public struct MyLibrary {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
+public class MyLibrary {
+    
+    public static var text = "Hello, World!"
+    
     public init() {
     }
+    
+    public func saludar(strName: String) -> String {
+        return "Hola \(strName)"
+    }
+    
 }
--- Tests/MyLibraryTests/MyLibraryTests.swift
+++ Tests/MyLibraryTests/MyLibraryTests.swift
@@ -2,10 +2,14 @@ import XCTest
 @testable import MyLibrary
 
 final class MyLibraryTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MyLibrary().text, "Hello, World!")
+    
+    func testVarExample() throws {
+        XCTAssertEqual(MyLibrary.text, "Hello, World!")
     }
+    
+    func testFuncSaludar() throws {
+        let libreria = MyLibrary()
+        XCTAssertEqual(libreria.saludar(strName: "Erwin"), "Hola Erwin")
+    }
+    
 }
